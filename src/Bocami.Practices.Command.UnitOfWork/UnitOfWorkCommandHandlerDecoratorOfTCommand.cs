d8ce9c4a4fa64d5ecc746233427c8c9cf63936bc
Jonatan Pedersen <jp@jonatanpedersen.com>
2014-07-19 14:18:30 +0200

    Initial import

---
+++ src/Bocami.Practices.Command.UnitOfWork/UnitOfWorkCommandHandlerDecoratorOfTCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Bocami.Practices.AbstractFactory;
+using Bocami.Practices.Decorator;
+using Bocami.Practices.UnitOfWork;
+
+namespace Bocami.Practices.Command.UnitOfWork
+{
+    public abstract class UnitOfWorkCommandHandlerDecorator<TCommand> : ICommandHandler<TCommand>, IDecorator<ICommandHandler<TCommand>>
+            where TCommand : class, ICommand
+    {
+        private readonly ICommandHandler<TCommand> commandHandler;
+        private readonly IAbstractFactory<IUnitOfWork> unitOfWorkFactory;
+
+        protected UnitOfWorkCommandHandlerDecorator(ICommandHandler<TCommand> commandHandler, IAbstractFactory<IUnitOfWork> unitOfWorkFactory)
+        {
+            if (commandHandler == null)
+                throw new ArgumentNullException("commandHandler");
+
+            if (unitOfWorkFactory == null)
+                throw new ArgumentNullException("unitOfWorkFactory");
+
+            this.commandHandler = commandHandler;
+            this.unitOfWorkFactory = unitOfWorkFactory;
+        }
+
+        public void Handle(TCommand command)
+        {
+            var unitOfWork = unitOfWorkFactory.Create();
+            
+            commandHandler.Handle(command);
+
+            unitOfWork.Commit();
+        }
+    }
+}
